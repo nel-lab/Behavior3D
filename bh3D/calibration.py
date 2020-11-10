91ccfd89e7de0ccb902fc080a6108f47cd51db45
Cynthia Dong <xcynthia@live.unc.edu>
2020-11-10 15:22:57 -0500

    added init

--- bh3D/calibration.py
+++ bh3D/calibration.py
@@ -12,8 +12,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import animation, rc
-from IPython.display import HTML
 
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
@@ -21,94 +19,11 @@ from sklearn.multioutput import MultiOutputRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_predict
 
-#%%
-class calibration():
-    def __init__(self, model, coordinatePath, regr_model=None):
-        self.model = model
-        self.regr_model = regr_model
-        self.coordinatePath = coordinatePath
-        if self.regr_model is None:
-            self.build_model()
-        print("This is the constructor method.")
-    
-    def plot_calibration_results():
-        #todo
-        1
-        
-    def build_model(self, long = False):
-        """ BUILD MODEL - uses 2D coordinates (probably obtained by a calibration experiment) to estimate coordinates in 3D
-        Params:
-        ------
-            coordinatePath: str
-                path to coordinate csv file, 
-                
-            model:list 
-                list of model IDs as strings ['fl'/'fr'/'bot'] 
-            
-            long: bool
-                whether to show results and MSE for SVM model
-                
-        Return:
-        -------
-            regr: sklearn SVM object 
-                trained SVM regressor that converts model cameras (2D) to X/Y/Z
-        """
-        
-        All_Data = pd.read_csv(self.coordinatePath)
-    
-        mod_col = []
-        for m in self.model:
-            mod_col.append(m.upper()+'_x')
-            mod_col.append(m.upper()+'_y')
-        mod_col += ['X','Y','Z']
-        All_Data = All_Data[mod_col]
-        
-        X_train_full, X_test, y_train_full, y_test = train_test_split( 
-                All_Data.iloc[:,:-3], All_Data.iloc[:,-3:], random_state=42,  test_size = 0.25)
-        
-        svm = SVR(kernel="poly", degree=2, C=1500, epsilon=0.01, gamma="scale")
-        regr = MultiOutputRegressor(svm)
-        regr.fit(X_train_full, y_train_full)
-    
-        if long: # other function
-            test_predictions = regr.predict(X_test)
-            test_MSE = mean_squared_error(test_predictions, y_test)
-            print('Test MSE: ' + str(test_MSE))
-            
-            X = All_Data.iloc[:,:-3]
-            Y = All_Data.iloc[:,-3:]
-            
-            CV_predictions = cross_val_predict(regr, X, Y, cv=10)
-            MSE = mean_squared_error(CV_predictions, Y)
-            print('CV MSE: ' + str(MSE))
-    
-            fig = plt.figure()
-            ax = fig.add_subplot(111, projection='3d')
-            ax.scatter(CV_predictions[:,0], CV_predictions[:,1], CV_predictions[:,2], color = 'blue', label = 'pred')
-            ax.scatter(All_Data.iloc[:,-3], All_Data.iloc[:,-2], All_Data.iloc[:,-1], color = 'red', label = 'real')
-            plt.legend()
-    
-        self.regr_model = regr
-        
-    def map_to_3D():
-        '''Transforms DLC outputs ffrom multiple cameras into a single 3D representaiton
-        
-        Parameters:
-        ----------
-        
-            
-        
-        Returns:
-        -------
-        
-        '''
-        if self.regr_model is None:
-            raise Exception()#message call build_model
-        
+from scipy.signal import medfilt
+from scipy import interpolate
 
 #%%
-    
-
+# helper functions
 
 def PreProcess_DLC_data(df):
     # PREPROCESS DLC
@@ -126,20 +41,19 @@ def PreProcess_DLC_data(df):
     df_proc = df_proc.astype(float).reset_index(drop=1)
         
     return df_proc
-#%%
-    
-# STANDARDIZE COLUMNS
-# input: list of preprocessed DLC dfs
-# output: list of standardized DLC dfs (only includes common bodyparts/columns), list of bodyparts/columns
 
 def Standardize_columns(dfs):
-   
+    # STANDARDIZE COLUMNS
+    # input: list of preprocessed DLC dfs
+    # output: list of standardized DLC dfs (only includes common bodyparts/columns), list of bodyparts/columns
+
     col = []
     for df in dfs:
         col.append(df.columns.tolist())
 
     same_col = reduce(np.intersect1d, col)
     if len(same_col) == 0:
+        # replace with raise Exception??
         print('no matching body parts amongst all cameras!')
         return None, None
     
@@ -155,9 +69,6 @@ def Standardize_columns(dfs):
 
     return dfs_stand, columns
 
-
-#%%
-
 def Predict_Real_Coordinates(dfs, bodyparts, numCameras, regr):
     # PREDICT X/Y/Z
     # input: list of standardized dfs, number of bodyparts, number of cameras, trained regressor
@@ -168,6 +79,7 @@ def Predict_Real_Coordinates(dfs, bodyparts, numCameras, regr):
     if numCameras == 3:
         df3 = dfs[2]
     elif numCameras > 3:
+        # replace with raise Exception??
         print('only works with 2 or 3 cameras, can implement more later')
         return
 
@@ -194,63 +106,189 @@ def Predict_Real_Coordinates(dfs, bodyparts, numCameras, regr):
         
     return Predictions
 
-#%%
-
-
-# RUN ENTIRE PIPELINE
-# input: path to coordinate csv, path to folder containing DLC csvs, list of model IDs as strings ['fl'/'fr'/'bot']
-# output: df of predicted X/Y/Z coordinates for common bodyparts in model
-
-def RunAll(cpath, dlcpath, model):
+def Andrea_filter(df):
     
-    # build model
-    model_regr = Build_Model(cpath, model)
+    Predictions = df.to_numpy()
 
-    # preproccess DLC data
-    front_left = pd.read_csv(dlcpath + '/front_left.csv')
-    front_right = pd.read_csv(dlcpath + '/front_right.csv') 
-    bot = pd.read_csv(dlcpath + '/bot.csv')
-    # back_left = pd.read_csv(dlcpath + '/back_left.csv')
-    # back_right = pd.read_csv(dlcpath + '/back_right.csv')
+    ids = np.arange(0,Predictions.shape[-1])
+    PredictionsFilt3 = medfilt(Predictions, kernel_size=(3,1))[:,ids]
+    PredictionsFilt5 = medfilt(Predictions, kernel_size=(5,1))[:,ids]
+    PredictionsFilt9 = medfilt(Predictions, kernel_size=(9,1))[:,ids]
 
-    mod_front_left = PreProcess_DLC_data(front_left)
-    mod_front_right = PreProcess_DLC_data(front_right)
-    mod_bot = PreProcess_DLC_data(bot)
-    # mod_back_left = PreProcess_DLC_data(back_left)
-    # mod_back_right = PreProcess_DLC_data(back_right)
+    tol_pix = 6
+    count = 0
+    for filt3, filt9 in zip(PredictionsFilt3.T,PredictionsFilt9.T):
+        PredictionOutl = np.where(np.abs(filt3-filt9)>tol_pix)[0]
+        PredictionGood = np.where(np.abs(filt3-filt9)<tol_pix)[0]
+        f = interpolate.interp1d(PredictionGood,filt3[PredictionGood], kind='cubic')
+        filt3[PredictionOutl]=f(PredictionOutl)
+        Predictions[:,count] = filt3
+        count+=1
 
-    # pull dfs based on model
-    dfs = []
-    for m in model:
-        if m.lower() == 'fl':
-            dfs.append(mod_front_left)
-        elif m.lower() == 'fr':
-            dfs.append(mod_front_right)
-        elif m.lower() == 'bot':
-            dfs.append(mod_bot)
-        elif m.lower() == 'bl':
-            dfs.append(mod_back_left)
-        elif m.lower() == 'br':
-            dfs.append(mod_back_right)
-        else:
-            print('model is not in form FL/FR/BOT/BL/BR')
+    df_filter = pd.DataFrame(Predictions, columns=df.columns)
+
+    return df_filter
+
+#%%
+class calibration():
+    def __init__(self, model, pathsDict, regr_model=None):
+        self.model = model
+        self.coordinatePath = pathsDict['coordPath']
+        self.DLCpath = pathsDict['DLCPath']
+        self.plotPath = pathsDict['PlotPath']
+        self.regr_model = regr_model
+        if self.regr_model is None:
+            self.build_model()
+        
+    def build_model(self):
+        """
+        Uses 2D coordinates (obtained by calibration experiment) to estimate coordinates in 3D
+        
+        Params:
+        ------
+            coordinatePath: str
+                path to coordinate csv file
+                
+            model: list 
+                list of model IDs as strings ['fl'/'fr'/'bot'] 
+            
+            long: bool
+                whether to show results and MSE for SVM model
+                
+        Returns:
+        -------
+            regr: sklearn SVM object 
+                trained SVM regressor that converts model cameras (2D) to X/Y/Z
+        """
+        
+        All_Data = pd.read_csv(self.coordinatePath)
+
+        mod_col = []
+        for m in self.model:
+            mod_col.append(m.upper()+'_x')
+            mod_col.append(m.upper()+'_y')
+        mod_col += ['X','Y','Z']
+        # add raise Exception if model not in coordinate csv??
+        All_Data = All_Data[mod_col]
+        
+        X_train_full, X_test, y_train_full, y_test = train_test_split( 
+                All_Data.iloc[:,:-3], All_Data.iloc[:,-3:], random_state=42,  test_size = 0.25)
+        
+        svm = SVR(kernel="poly", degree=2, C=1500, epsilon=0.01, gamma="scale")
+        regr = MultiOutputRegressor(svm)
+        regr.fit(X_train_full, y_train_full)
+
+        self.regr_model = regr
+        print('Model Built\n-----------')
+
+    def calibration_results(self, save=False):
+
+        All_Data = pd.read_csv(self.coordinatePath)
+
+        mod_col = []
+        for m in self.model:
+            mod_col.append(m.upper()+'_x')
+            mod_col.append(m.upper()+'_y')
+        mod_col += ['X','Y','Z']
+        All_Data = All_Data[mod_col]
+        
+        X_train_full, X_test, y_train_full, y_test = train_test_split( 
+                All_Data.iloc[:,:-3], All_Data.iloc[:,-3:], random_state=42,  test_size = 0.25)
+
+        test_predictions = self.regr_model.predict(X_test)
+        test_MSE = mean_squared_error(test_predictions, y_test)
+        print('\nCalibration Results\n-------------------')
+        print('\tTest MSE: ' + str(test_MSE))
+            
+        X = All_Data.iloc[:,:-3]
+        Y = All_Data.iloc[:,-3:]
+            
+        CV_predictions = cross_val_predict(self.regr_model, X, Y, cv=10)
+        MSE = mean_squared_error(CV_predictions, Y)
+        print('\tCV MSE: ' + str(MSE))
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(CV_predictions[:,0], CV_predictions[:,1], CV_predictions[:,2], color = 'blue', label = 'pred')
+        ax.scatter(All_Data.iloc[:,-3], All_Data.iloc[:,-2], All_Data.iloc[:,-1], color = 'red', label = 'real')
+        ax.set_title('Calibration Results')
+        ax.set_xlabel('X (mm)')
+        ax.set_ylabel('Y (mm)')
+        ax.set_zlabel('Z (mm)')
+        plt.legend()
+
+        if save:
+            if not os.path.isdir(self.plotPath):
+                os.mkdir(self.plotPath)
+                print('\nPlots Folder Created')
+
+            plt.savefig(self.plotPath+'/calibration results')
+            print('\nCalibration Results Saved')
+        
+        plt.show()
+
+    def raw_model(self):
+        '''
+        Transforms DLC outputs from multiple cameras into a single 3D representation
+        
+        Params:
+        ------
+        
+        Returns:
+        -------
+        
+        '''
+
+        # RUN ENTIRE PIPELINE
+        # input: path to coordinate csv, path to folder containing DLC csvs, list of model IDs as strings ['fl'/'fr'/'bot']
+        # output: df of predicted X/Y/Z coordinates for common bodyparts in model
+           
+        # preproccess and pull DLC data based on model
+        dfs = []
+        for m in self.model:
+            if m.lower() == 'fl':
+                df = pd.read_csv(self.DLCpath + '/front_left.csv')
+            elif m.lower() == 'fr':
+                df = pd.read_csv(self.DLCpath + '/front_right.csv') 
+            elif m.lower() == 'bot':
+                df = pd.read_csv(self.DLCpath + '/bot.csv')
+            elif m.lower() == 'bl':
+                df = pd.read_csv(self.DLCpath + '/back_left.csv')
+            elif m.lower() == 'br':
+                df = pd.read_csv(self.DLCpath + '/back_right.csv')
+            else:
+                # replace with raise Exception??
+                print('model is not in form FL/FR/BOT/BL/BR')
+                return
+
+            mod_df = PreProcess_DLC_data(df)
+            dfs.append(mod_df)
+
+        # standardize dfs
+        dfs_stand, columns = Standardize_columns(dfs)
+        if not dfs_stand or not columns:
+            # replace with raise Exception??
             return
 
-    # standardize dfs
-    dfs_stand, columns = Standardize_columns(dfs)
-    if not dfs_stand or not columns:
-        return
+        # predict X/Y/Z for all bodyparts
+        bodyparts = dfs_stand[0].shape[1]//2
+        numCameras = len(dfs_stand)
+        m1 = Predict_Real_Coordinates(dfs_stand, bodyparts, numCameras, self.regr_model)
+        m1df = pd.DataFrame(m1)
+        m1df.columns = columns
 
-    # predict X/Y/Z for all bodyparts
-    bodyparts = dfs_stand[0].shape[1]//2
-    numCameras = len(dfs_stand)
-    m1 = Predict_Real_Coordinates(dfs_stand, bodyparts, numCameras, model_regr)
-    m1df = pd.DataFrame(m1)
-    m1df.columns = columns
+        self.raw_results = m1df
 
-    # remove tail from models with only 2 cameras since 3 camera model should be better
-    if numCameras == 2 and 'tail' in [c[:4].lower() for c in columns]:
-        cols = [c for c in m1df.columns if c.lower()[:4] != 'tail']
-        m1df=m1df[cols]
+        return self.raw_results
 
-    return m1df
+    def map_to_3D(self, filter=Andrea_filter, raw=None):
+
+        if raw is None:
+            if not hasattr(self, 'raw_results'):
+                self.raw_model()
+            
+            raw = self.raw_results
+
+        self.filtered_results = filter(raw)
+
+        return self.filtered_results
