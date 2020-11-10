0b5153b71ec8aea9bd966ccfed9f9eb10ca14a19
Cynthia Dong <xcynthia@live.unc.edu>
2020-11-10 15:47:10 -0500

    refactor main analysis

--- use_cases/AnalysisExample.py
+++ use_cases/AnalysisExample.py
@@ -8,17 +8,31 @@ Created on Thu Oct 29 13:35:53 2020
 
 
 from bh3D.calibration import calibration
+import pandas as pd
+import numpy as np
+
 #%%
 #Customize this paths dictionary for your local paths. 
 pathsDict = {
-    'coordPath': '/media/nel-lab/Lexar/Calibration/My Files/My Plotting Files/coords and dlc/model_1_coordinates.csv', 
-    'DLCPath': 'D:\data\Behavior data\dlc_data_201020\coords and dlc',
-    'PlotPath' : 'D:\data\Behavior data\dlc_data_201020\Plotting files'
+    'coordPath': '/Users/jimmytabet/Desktop/NEL/Projects/Calibration/My Files/My Plotting Files/coords and dlc/model_1_coordinates.csv', 
+    'DLCPath': '/Users/jimmytabet/Desktop/NEL/Projects/Calibration/My Files/My Plotting Files/coords and dlc',
+    'PlotPath' : '/Users/jimmytabet/Desktop/bh3d tests'
 }
 
-cal = calibration(model=['bot','fl','fr'])
-cal.build_model(pathsDict['coordPath'], long=True)
+cal = calibration(model = ['bot','fl','fr'], pathsDict = pathsDict)
+# cal.calibration_results(save=True)
 
-positions = cal.map_to_3D()
+print()
+# raw data
+raw = cal.raw_model()
+# print(raw.head())
 
+# filtered data
+new = cal.map_to_3D()
+# print(filtered.head())
 
+# load version from JN
+old = pd.read_csv('/Users/jimmytabet/Desktop/NEL/Projects/Calibration/My Files/My Plotting Files/trial 17/two paws XYZ data.csv')
+
+# any difference is result of rounding, maybe because it was read from csv file
+print('max diff:', np.max(np.max(new-old)))
