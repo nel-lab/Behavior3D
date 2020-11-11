bec87702638088d003ebb7b680c6535f316642a5
Cynthia Dong <xcynthia@live.unc.edu>
2020-11-11 15:26:46 -0500

    added wavelet analyses

--- use_cases/AnalysisExample.py
+++ use_cases/AnalysisExample.py
@@ -43,7 +43,28 @@ new -= means
 freqs, power, X_new = wavelet_transform(new.values, n_freqs=25, fsample=100., fmin=1., fmax=50.)
 #%%
 plt.subplot(2,1,1)
-plt.plot(X_new[:,:3])
-plt.plot(X_new[:,10:13])
+plt.plot(X_new[:,:12])
+plt.plot(X_new[:,12:25])
 plt.subplot(2,1,2)
 plt.plot(new.values[:,:1]/new.values[:,:1].max())
+#%%
+plt.imshow(X_new.T, aspect='auto')
+plt.figure()
+plt.plot(new.values[:,1])
+#%%
+from sklearn.decomposition import PCA, NMF
+pca = NMF(n_components=2, max_iter=500)
+scores = pca.fit_transform(X_new)
+#%%
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.scatter(*scores[800::10].T, c='c', marker='o', alpha=0.1)
+ax.scatter(*scores[50:400:10].T, c='r', marker='o')
+ax.scatter(*scores[400:600:10].T, c='b', marker='o')
+ax.scatter(*scores[600:800:10].T, c='g', marker='o')
+
+#%%
+plt.plot(scores*100)
+#%%
+plt.figure()
+plt.imshow(new.values[:,:].T, aspect='auto')
