1add8fab21594444f276ed318402bd39281ee9ca
Cynthia Dong <xcynthia@live.unc.edu>
2020-11-11 10:49:40 -0500

    added some analyses

--- use_cases/AnalysisExample.py
+++ use_cases/AnalysisExample.py
@@ -10,13 +10,15 @@ Created on Thu Oct 29 13:35:53 2020
 from bh3D.calibration import calibration
 import pandas as pd
 import numpy as np
+from behavelet import wavelet_transform
+import pylab as plt
 
 #%%
 #Customize this paths dictionary for your local paths. 
 pathsDict = {
-    'coordPath': '/Users/jimmytabet/Desktop/NEL/Projects/Calibration/My Files/My Plotting Files/coords and dlc/model_1_coordinates.csv', 
-    'DLCPath': '/Users/jimmytabet/Desktop/NEL/Projects/Calibration/My Files/My Plotting Files/coords and dlc',
-    'PlotPath' : '/Users/jimmytabet/Desktop/bh3d tests'
+    'coordPath': './use_cases/setup_old/model_1_coordinates.csv', 
+    'DLCPath': './use_cases/setup_old/',
+    'PlotPath' : './use_cases/setup_old/'
 }
 
 cal = calibration(model = ['bot','fl','fr'], pathsDict = pathsDict)
@@ -30,9 +32,18 @@ raw = cal.raw_model()
 # filtered data
 new = cal.map_to_3D()
 # print(filtered.head())
-
+means = new.mean(axis=0)
+new -= means
 # load version from JN
-old = pd.read_csv('/Users/jimmytabet/Desktop/NEL/Projects/Calibration/My Files/My Plotting Files/trial 17/two paws XYZ data.csv')
+#old = pd.read_csv('./use_cases/setup_old/two paws XYZ data.csv')
 
 # any difference is result of rounding, maybe because it was read from csv file
-print('max diff:', np.max(np.max(new-old)))
+#print('max diff:', np.max(np.max(new-old)))
+#%% wavelet analysis
+freqs, power, X_new = wavelet_transform(new.values, n_freqs=25, fsample=100., fmin=1., fmax=50.)
+#%%
+plt.subplot(2,1,1)
+plt.plot(X_new[:,:3])
+plt.plot(X_new[:,10:13])
+plt.subplot(2,1,2)
+plt.plot(new.values[:,:1]/new.values[:,:1].max())
